3961d3d987134421de2223959545a43b9cab34c1
Noushin Iqra <noushinahmed95@gmail.com>
2017-02-20 13:18:57 -0500

    Update calculations1.cpp

--- calculations1.cpp
+++ calculations1.cpp
@@ -1,3 +1,8 @@
+// This program generates 20 random numbers from 60 to 100, each time it executes. And prints out each number along with the 
+// the minimum and maximum number, average of all 20 numbers, the smallest gap between 2 numbers, and the sum of total gaps 
+// between all the numbers.
+// @Noushin Iqra
+
 #include <iostream>
 #include <cmath>
 #include <ctime>
@@ -10,7 +15,7 @@ int findMax(int a[], int cap) {
         if (a[e]>ans)
         ans = a[e];
     return ans;
-}
+}//findMax: return highest number
 
 int findMin(int a[], int cap) {
     int ans=a[0];
@@ -18,14 +23,14 @@ int findMin(int a[], int cap) {
         if (a[e]<ans)
         ans = a[e];
     return ans;
-}
+}//findMin: return lowest number
 
 double findAverage(int a[], int cap) {
     int total=0;
     for (int e=0; e<cap; e++)
         total = total+a[e];
     return total/((double)cap);
-}
+}//findAverage: return average of total numbers
 
 int findMinGap(int a[], int cap) {
     int gap=0, count=1, mingap=(a[0]-a[1]);
@@ -41,7 +46,7 @@ int findMinGap(int a[], int cap) {
         count++;
     }
     return mingap;
-}
+}//findMinGap: return smallest gap between 2 numbers
 
 int findGapSum(int a[], int c) {
     int gap, count=1, sum=0;
@@ -54,7 +59,7 @@ int findGapSum(int a[], int c) {
         sum=sum+gap;
     }
     return sum;
-}
+}//findGapsum: return sum of total gaps between all numbers
 
 int main()
 {
@@ -62,10 +67,12 @@ int main()
     		int x[20];
     		for (int e=0; e<20; e++) {
         		x[e]= rand()%41+60;
-    		}
+    		}//generate 20 random numbers and store in an array
+	
     			for (int e=0; e<20; e++) {
         			cout << x[e] << " ";
-    			}
+    			}//print each number in the array
+	
 			cout << endl; 
    				cout << "The largest # of the array is: " << findMax(x,20) << endl;
    				cout << "The smallest # of the array is: " << findMin(x,20) << endl;
